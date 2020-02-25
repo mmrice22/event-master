21ef5085357e64f93f1b063da9b33fd49cc422dc
Roniya Rojan <roniyarojan24@gmail.com>
2020-02-25 10:04:27 -0500

    Button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
+  public show: boolean = false;
+  public buttonName: any = "Show";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  toggle() {
+    this.show = !this.show;
+
+    if (this.show) this.buttonName = "Hide";
+    else this.buttonName = "Show";
   }
-
 }
